c1deeeee546589e26974a541e7754f4e1777bf94
Denton Liu <liu.denton@gmail.com>
2020-01-22 16:18:46 -0500

    Create (untested) libascon

---
+++ projects/briey/libs/ascon.h
@@ -0,0 +1,27 @@
+#ifndef ASCON_H
+#define ASCON_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define BITS_TO_UINT32(b) ((b) / (sizeof(uint32_t) * 8))
+
+typedef uint32_t ascon_key_t[BITS_TO_UINT32(128)];
+typedef uint32_t ascon_nonce_t[BITS_TO_UINT32(128)];
+
+struct ascon_param {
+	ascon_key_t key;
+	const uint32_t *(associated[2]);
+	size_t associated_size;
+	const uint32_t *(data_in[2]);
+	size_t data_size;
+	uint32_t *(data_out[2]);
+	ascon_nonce_t nonce;
+};
+
+int ascon_is_sane(void);
+int ascon_is_busy(void);
+void ascon_encrypt(struct ascon_param *param);
+void ascon_decrypt(struct ascon_param *param);
+
+#endif /* define ASCON_H */
