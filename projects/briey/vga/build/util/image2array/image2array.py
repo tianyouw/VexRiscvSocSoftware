33e1794598d393fdfc22e04f88e13950201f1693
Denton Liu <liu.denton@gmail.com>
2020-01-29 17:02:42 -0500

    image2array: return array in rgb65 form

--- projects/briey/vga/build/util/image2array/image2array.py
+++ projects/briey/vga/build/util/image2array/image2array.py
@@ -3,24 +3,26 @@
 from PIL import Image
 import sys
 
+def convert_888_to_565(pixel):
+    red, green, blue = pixel
+    red >>= 3
+    green >>= 2
+    blue >>= 3
+
+    return (red << 11) | (green << 5) | (blue << 0)
+
 def generate_struct(imagedata, width):
     imagedata = list(imagedata)
     return ',\n'.join(
             '\t{\n%s\n\t}' % (',\n'.join(
-                '\t\t{%d, %d, %d}' % pixel for pixel in imagedata[i:i+width]))
+                '\t\t0x%04x' % convert_888_to_565(pixel) for pixel in imagedata[i:i+width]))
             for i in range(0, len(imagedata), width))
 
 def generate_array(image):
     width, height = image.size
     print("""#include <stdint.h>
 
-struct rgb {
-\tuint8_t red;
-\tuint8_t green;
-\tuint8_t blue;
-};
-
-static struct rgb image[%d][%d] = {
+static uint16_t image[%d][%d] = {
 %s
 };""" % (width, height, generate_struct(image.getdata(), width)))
 
